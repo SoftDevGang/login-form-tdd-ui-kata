5bd49b33eacd1e32a66ae7ec450236b46273a231
Peter Kofler <peter.kofler@code-cop.org>
2019-09-30 00:21:38 +0200

    Red: view displays error.

--- Swing/src/test/java/org/codecop/loginform/SwingLoginViewTest.java
+++ Swing/src/test/java/org/codecop/loginform/SwingLoginViewTest.java
@@ -6,9 +6,11 @@ import static org.mockito.Mockito.verify;
 import java.awt.Color;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import abbot.finder.ComponentSearchException;
 import abbot.finder.matchers.NameMatcher;
@@ -106,6 +108,29 @@ public class SwingLoginViewTest extends ComponentTestFixture {
 
     private JPasswordField findPasswordField() throws ComponentSearchException {
         return (JPasswordField) getFinder().find(new NameMatcher("PasswordField"));
+    }
+
+    // --- error
+
+    public void testErrorDisplayWithStyle() throws ComponentSearchException {
+        showFrame((JPanel) view);
+        JLabel errorField = findErrorField();
+
+        assertEquals("", errorField.getText());
+        assertNull(errorField.getBorder());
+        assertNull(errorField.getIcon());
+
+        view.showError("Alert!");
+
+        assertEquals("Alert!", errorField.getText());
+        assertEquals(Color.RED, errorField.getForeground());
+        assertNotNull(errorField.getIcon());
+        Border errorBorder = errorField.getBorder();
+        assertNotNull(errorBorder);
+    }
+
+    private JLabel findErrorField() throws ComponentSearchException {
+        return (JLabel) getFinder().find(new NameMatcher("ErrorField"));
     }
 
 }
