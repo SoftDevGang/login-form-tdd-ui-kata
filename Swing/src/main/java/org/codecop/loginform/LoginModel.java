87401561fea841924ca68001758100a1aa8e3f66
Peter Kofler <peter.kofler@code-cop.org>
2019-09-29 22:16:07 +0200

    GREEN.

--- Swing/src/main/java/org/codecop/loginform/LoginModel.java
+++ Swing/src/main/java/org/codecop/loginform/LoginModel.java
@@ -2,20 +2,23 @@ package org.codecop.loginform;
 
 public class LoginModel {
 
+    private String lookup;
+    private String password;
+
     public void setLookup(String lookup) {
-
-    }
-
-    public void setPassword(String password) {
-
+        this.lookup = lookup;
     }
 
     public String getLookup() {
-        return null;
+        return lookup;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 
 }
