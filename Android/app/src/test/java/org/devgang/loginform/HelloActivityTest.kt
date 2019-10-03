4a24949fc379df0f3101acb76b7fdabfa2ac9004
Bastien DAVID <bastien.david@orange.com>
2019-10-03 17:23:47 +0300

    Added empty project with starter for Android
    The project shows how to do several kind of tests on a demo activity.
    The MainActivity can be used to do the real implementation

---
+++ Android/app/src/test/java/org/devgang/loginform/HelloActivityTest.kt
@@ -0,0 +1,24 @@
+package org.devgang.loginform
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HelloActivityTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<HelloActivity>()
+
+    @Test
+    fun hello_activity_has_some_hello_text() {
+        onView(withId(R.id.text_hello)).check(matches(withText("Hello!")))
+    }
+
+}
