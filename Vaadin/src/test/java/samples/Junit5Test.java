4e3303e6fb8c5e00e11a49abe28d80d052406afd
Peter Kofler <peter.kofler@code-cop.org>
2019-10-27 17:31:51 +0100

    Sample tests and security service copied from Swing setup.

---
+++ Vaadin/src/test/java/samples/Junit5Test.java
@@ -0,0 +1,13 @@
+package samples;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Junit5Test {
+
+    @Test
+    void shouldAssertPlainJUnit() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
