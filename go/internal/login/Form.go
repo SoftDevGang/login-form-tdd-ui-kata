f848eec336e14dbe28371831c77bf5d830590cf5
Christian Haas <christian.haas@sevensuns.at>
2019-10-28 11:11:46 +0100

    Sketch Raylib implementation of wrapper.

--- go/internal/login/Form.go
+++ go/internal/login/Form.go
@@ -1,7 +1,18 @@
 package login
 
+import (
+	"github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
 type FormUI interface {
 	Button(label string) bool
+}
+
+type RaylibFormUI struct{}
+
+func (ui *RaylibFormUI) Button(label string) bool {
+	return raygui.Button(rl.Rectangle{}, label)
 }
 
 // Form renders the controls for providing the necessary login credentials.
