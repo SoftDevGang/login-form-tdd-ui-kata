20ab14c2cce051f4224e9f4614741be822b1c474
Christian Haas <christian.haas@sevensuns.at>
2019-10-28 14:48:41 +0100

    Extract bounds to help readability.

--- go/internal/login/Form.go
+++ go/internal/login/Form.go
@@ -34,23 +34,26 @@ type Form struct {
 }
 
 func (form *Form) Render(ui FormUI) {
-	ui.Label("username", rl.Rectangle{}, "Phone, email or username")
-	form.UserName = ui.TextBox("username", rl.Rectangle{
-		X:      0,
-		Y:      40,
-		Width:  100,
-		Height: 30,
-	}, form.UserName)
+	userNameLabelBounds := rl.Rectangle{}
+	ui.Label("username", userNameLabelBounds, "Phone, email or username")
+
+	// TODO move out bounds to map of [id]bounds into UI type.
+	userNameBounds := rl.Rectangle{X: 0, Y: 40, Width: 100, Height: 30}
+	// Idea is to move out the bounds into the ui, because the id gives a 1:1 lookup
+	// on the bounds. We do not assert the bounds in unit test, so we do not need to
+	// have them here. We said it's styling and will be asserted using a single screen shot
+	// for the whole form.
+	// Maybe moving out stuff is against IM nature because the whole UI is to be in code.
+	// Are we allowed to delegate styling.
+
+	form.UserName = ui.TextBox("username", userNameBounds, form.UserName)
 	if len(form.UserName) > UserNameLimit {
 		form.UserName = form.UserName[:UserNameLimit]
 	}
 
-	form.Password = ui.TextBox("password", rl.Rectangle{
-		X:      0,
-		Y:      80,
-		Width:  100,
-		Height: 30,
-	}, form.Password)
+	passwordBounds := rl.Rectangle{X: 0, Y: 80, Width: 100, Height: 30}
+	form.Password = ui.TextBox("password", passwordBounds, form.Password)
 
-	ui.Button("login", rl.Rectangle{235, 165, 345, 195}, "Log in")
+	buttonBounds := rl.Rectangle{X: 235, Y: 165, Width: 345, Height: 195}
+	ui.Button("login", buttonBounds, "Log in")
 }
