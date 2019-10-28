5211de9c2d8d3bf23fc4c7940b2004ca30f7c72b
Christian Haas <christian.haas@sevensuns.at>
2019-10-28 10:46:39 +0100

    refactoring, removed unused code

--- go/internal/login/Form.go
+++ go/internal/login/Form.go
@@ -1,14 +1,12 @@
 package login
 
-import rl "github.com/gen2brain/raylib-go/raylib"
-
 type FormUI interface {
-	Button(bounds rl.Rectangle, text string) bool
+	Button() bool
 }
 
 // Form renders the controls for providing the necessary login credentials.
 type Form struct{}
 
 func (form Form) Render(ui FormUI) {
-	ui.Button(rl.Rectangle{}, "")
+	ui.Button()
 }
--- go/internal/login/Form_test.go
+++ go/internal/login/Form_test.go
@@ -3,23 +3,21 @@ package login_test
 import (
 	"testing"
 
-	rl "github.com/gen2brain/raylib-go/raylib"
-
 	"github.com/SoftDevGang/login-form-tdd-ui-kata/go/internal/login"
 )
 
-type MockedUI struct {
+type TestingUI struct {
 	buttonCalled bool
 }
 
-func (ui *MockedUI) Button(bounds rl.Rectangle, text string) bool {
+func (ui *TestingUI) Button() bool {
 	ui.buttonCalled = true
 	return false
 }
 
-func TestLoginButtonOnForm(t *testing.T) {
+func TestFormHasLoginButton(t *testing.T) {
 	var form login.Form
-	var ui MockedUI
+	var ui TestingUI
 	form.Render(&ui)
 	if !ui.buttonCalled {
 		t.Errorf("Button() was not called")
