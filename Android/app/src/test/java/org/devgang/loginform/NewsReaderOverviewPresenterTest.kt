289a4a0e7e6ccb8467f82901cf53c6e05eb14c96
zcolakovic <z.colakovic@sportradar.com>
2020-09-29 13:47:12 +0200

    Refactor rename test.

--- Android/app/src/test/java/org/devgang/loginform/NewsReaderOverviewPresenterTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/NewsReaderOverviewPresenterTest.kt
@@ -4,12 +4,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class NewsReaderOverviewTest {
+class NewsReaderOverviewPresenterTest {
     @Test
     fun should_set_no_results_for_no_news_items() {
         val overviewUi = DummyOverviewUi()
