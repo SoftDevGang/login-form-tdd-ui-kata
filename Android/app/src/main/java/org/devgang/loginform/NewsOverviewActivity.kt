0892786dd3ef076d1c86b71e186a46a6c41c64eb
zcolakovic <z.colakovic@sportradar.com>
2020-09-29 14:49:10 +0200

    Red: Nothing found message.

--- Android/app/src/main/java/org/devgang/loginform/NewsOverviewActivity.kt
+++ Android/app/src/main/java/org/devgang/loginform/NewsOverviewActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class NewsOverviewActivity : AppCompatActivity() {
+class NewsOverviewActivity : AppCompatActivity(), OverviewUi {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_overview)
 
         findViewById<TextView>(R.id.newsPageHeadline).text = "Today's News!"
+    }
+
+    override fun displayNoResultsFound() {
+    }
+
+    override fun setViewModel(items: List<OverviewItemViewModel>) {
     }
 }
--- Android/app/src/test/java/org/devgang/loginform/NewsOverviewActivityTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/NewsOverviewActivityTest.kt
@@ -1,11 +1,14 @@
 package org.devgang.loginform
 
+import android.app.Activity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,8 +19,27 @@ class NewsOverviewActivityTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<NewsOverviewActivity>()
 
+    /*
+     * https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
+     */
+    private fun getActivity(): Activity? {
+        var activity: Activity? = null
+        activityScenarioRule.scenario.onActivity {
+            activity = it
+        }
+        return activity
+    }
+
     @Test
     fun should_show_news_headline() {
         onView(withId(R.id.newsPageHeadline)).check(matches(withText("Today's News!")))
+    }
+
+    @Test
+    fun should_display_nothing_found() {
+        val newsOverviewActivity = getActivity() as NewsOverviewActivity
+        newsOverviewActivity.displayNoResultsFound()
+        val recyclerView: RecyclerView = newsOverviewActivity.findViewById(R.id.newsContainer)
+        Assert.assertEquals(recyclerView.adapter!!.itemCount, 1)
     }
 }
