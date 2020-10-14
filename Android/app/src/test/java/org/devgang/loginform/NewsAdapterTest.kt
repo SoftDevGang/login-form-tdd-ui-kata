8dde6e678bda3e16d5798f0f17698a49e3faa1e4
Marko Djekic <m.djekic@sportradar.com>
2020-10-14 17:16:26 +0200

    WIP End of session.

--- Android/app/src/test/java/org/devgang/loginform/NewsAdapterTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/NewsAdapterTest.kt
@@ -19,17 +19,30 @@ class NewsAdapterTest {
         val itemCount = newsAdapter.itemCount
         Assert.assertEquals(0, itemCount)
     }
+
+//    @Test
+//    fun should_return_empty_view_holder_for_empty_model() {
+//        val newsModel = NewsModel(arrayOf())
+//        val newsAdapter = NewsAdapter()
+//        newsAdapter.setData(newsModel)
+//
+//        val viewHolder = newsAdapter.onCreateViewHolder()
+//        viewHolder.itemViewType
+//        newsAdapter.bindViewHolder(viewHolder,0)
+//        Assert.assertEquals(0, itemCount)
+//    }
 }
 
 class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var newsModel = NewsModel(emptyArray())
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        if(newsModel.items.isEmpty())
         TODO()
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+        TODO()
     }
 
     override fun getItemCount(): Int {
