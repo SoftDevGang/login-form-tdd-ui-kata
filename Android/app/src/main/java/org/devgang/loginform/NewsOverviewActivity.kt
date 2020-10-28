416d9e75227ac6395196454e8b5bdb90787ae858
c.frank <c.frank@sportradar.com>
2020-10-28 18:11:16 +0100

    Displays "no news" as expected, but test does not work (wip).

--- Android/app/src/main/java/org/devgang/loginform/NewsOverviewActivity.kt
+++ Android/app/src/main/java/org/devgang/loginform/NewsOverviewActivity.kt
@@ -3,6 +3,8 @@ package org.devgang.loginform
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class NewsOverviewActivity : AppCompatActivity(), OverviewUi {
 
@@ -10,6 +12,10 @@ class NewsOverviewActivity : AppCompatActivity(), OverviewUi {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_overview)
         findViewById<TextView>(R.id.newsPageHeadline).text = "Today's News!"
+
+        val newsListView = findViewById<RecyclerView>(R.id.newsContainer)
+        newsListView.layoutManager = LinearLayoutManager(this)
+        newsListView.adapter = NewsAdapter()
     }
 
     override fun displayNoResultsFound() {
--- Android/app/src/test/java/org/devgang/loginform/NewsOverviewActivityTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/NewsOverviewActivityTest.kt
@@ -1,15 +1,12 @@
 package org.devgang.loginform
 
 import android.app.Activity
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,11 +34,15 @@ class NewsOverviewActivityTest {
     }
 
     @Test
-    @Ignore("work on adapter before we can use it here")
     fun should_display_nothing_found() {
         val newsOverviewActivity = getActivity() as NewsOverviewActivity
         newsOverviewActivity.displayNoResultsFound()
-        val recyclerView: RecyclerView = newsOverviewActivity.findViewById(R.id.newsContainer)
-        Assert.assertEquals(recyclerView.adapter!!.itemCount, 1)
+//        val recyclerView: RecyclerView = newsOverviewActivity.findViewById(R.id.newsContainer)
+//        Assert.assertEquals(1,recyclerView.adapter!!.itemCount)
+        /*
+        UI is working as expected, but view matching is not correct
+         */
+
+        onView(withId(R.id.newsContainer)).check(matches(withText("No news")))
     }
 }
