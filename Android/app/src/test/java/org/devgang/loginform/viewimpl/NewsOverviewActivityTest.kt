85bcaa746502f004660d203883ab7dffb2ca6109
Peter Kofler <peter.kofler@code-cop.org>
2020-10-31 00:22:17 +0100

    Fix matching, now the test is green.

--- Android/app/src/test/java/org/devgang/loginform/viewimpl/NewsOverviewActivityTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/viewimpl/NewsOverviewActivityTest.kt
@@ -1,13 +1,18 @@
 package org.devgang.loginform.viewimpl
 
 import android.app.Activity
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.devgang.loginform.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,9 +23,7 @@ class NewsOverviewActivityTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<NewsOverviewActivity>()
 
-    /*
-     * https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
-     */
+    // https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
     private fun getActivity(): Activity? {
         var activity: Activity? = null
         activityScenarioRule.scenario.onActivity {
@@ -46,7 +49,24 @@ class NewsOverviewActivityTest {
         // Assert.assertEquals(1,recyclerView.adapter!!.itemCount)
         // This is correct but does not help to display something - redo test and go deeper
 
-        // TODO: UI is working as expected, but view matching is not correct
-        onView(withId(R.id.newsContainer)).check(matches(withText("No news")))
+        onView(withId(R.id.newsContainer)).check(matches(atPosition(0, withText("No news"))))
+        // onView(withId(R.id.newsContainer)).check(matches(atPosition(0, hasDescendant(withText("No news")))))
+    }
+
+    // see https://stackoverflow.com/a/34795431/104143
+    private fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+        }
     }
 }
