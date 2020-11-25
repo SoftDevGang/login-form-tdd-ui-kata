38a38cecb7787bfdc3aee6cf78572826e280a985
c.frank <c.frank@sportradar.com>
2020-11-25 16:12:22 +0100

    Refactor: cleanup NewsNetworkDownloaderTest.kt

--- Android/app/src/test/java/org/devgang/loginform/network/NewsNetworkDownloaderTest.kt
+++ Android/app/src/test/java/org/devgang/loginform/network/NewsNetworkDownloaderTest.kt
@@ -1,58 +1,59 @@
 package org.devgang.loginform.network
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.reactivex.observers.TestObserver
+import okhttp3.HttpUrl
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.devgang.loginform.model.NewsItem
 import org.devgang.loginform.model.NewsModel
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.net.HttpURLConnection
 
-@RunWith(AndroidJUnit4::class) // TODO remove @RunWith, this is a plain unit test
 class NewsNetworkDownloaderTest {
 
-    // TODO move mock server in before and after of test
+    private val mockServer = MockWebServer()
+    private val testObserver = TestObserver<NewsModel>()
+
+    @Before
+    fun startServer() {
+        mockServer.start(8080)
+    }
 
     @Test
     fun should_download_no_news() {
-        val mockServer = MockWebServer()
-        mockServer.start(8080)
-        mockServer.enqueue(MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody("[]"))
-        val testUrl = mockServer.url("/results.json")
-
+        val testUrl = prepareMockServer("[]")
         val newsNetworkDownloader = NewsNetworkDownloader(testUrl.toString())
 
-        val testObserver = TestObserver<NewsModel>()
         newsNetworkDownloader.newsUpdates().subscribe(testObserver)
 
         testObserver.awaitCount(1)
         testObserver.assertValue(NewsModel(emptyArray()))
-
-        testObserver.dispose()
-        mockServer.shutdown()
     }
 
     @Test
     fun should_download_single_news() {
-        val mockServer = MockWebServer()
-        mockServer.start(8080)
-        mockServer.enqueue(
-            MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
-                .setBody("[{\"title\":\"foo\"}]")
-        )
-        val testUrl = mockServer.url("/results.json")
-
+        val testUrl = prepareMockServer("[{\"title\":\"foo\"}]")
         val newsNetworkDownloader = NewsNetworkDownloader(testUrl.toString())
 
-        val testObserver = TestObserver<NewsModel>()
         newsNetworkDownloader.newsUpdates().subscribe(testObserver)
 
         testObserver.awaitCount(1)
         testObserver.assertValue(NewsModel(arrayOf(NewsItem("foo"))))
+    }
 
-        testObserver.dispose()
+    private fun prepareMockServer(body: String): HttpUrl {
+        mockServer.enqueue(
+            MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(body)
+        )
+        return mockServer.url("/results.json")
+    }
+
+    @After
+    fun stopServer() {
         mockServer.shutdown()
+        testObserver.dispose()
     }
 }
