9d8ab390d96ec388aeb39168a507d211bcf897e0
c.frank <c.frank@sportradar.com>
2020-11-25 15:45:34 +0100

    Clean up generated equals: braces, cast, simplify return.

--- Android/app/src/main/java/org/devgang/loginform/model/NewsItem.kt
+++ Android/app/src/main/java/org/devgang/loginform/model/NewsItem.kt
@@ -2,15 +2,11 @@ package org.devgang.loginform.model
 
 class NewsItem(val title: String) {
     override fun equals(other: Any?): Boolean {
-        // TODO clean up generated equals: braces, cast, simplify return
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (javaClass != other?.javaClass) {
+            return false
+        }
 
-        other as NewsItem
-
-        if (title != other.title) return false
-
-        return true
+        return title == (other as NewsItem).title
     }
 
     override fun hashCode(): Int {
--- Android/app/src/main/java/org/devgang/loginform/model/NewsModel.kt
+++ Android/app/src/main/java/org/devgang/loginform/model/NewsModel.kt
@@ -2,15 +2,11 @@ package org.devgang.loginform.model
 
 class NewsModel(val items: Array<NewsItem>) {
     override fun equals(other: Any?): Boolean {
-        // TODO clean up generated equals: braces, cast, simplify return
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (javaClass != other?.javaClass) {
+            return false
+        }
 
-        other as NewsModel
-
-        if (!items.contentEquals(other.items)) return false
-
-        return true
+        return items.contentEquals((other as NewsModel).items)
     }
 
     override fun hashCode(): Int {
