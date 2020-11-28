d9617abe7a7d6ccb1770569798abe24cc6bed892
Peter Kofler <peter.kofler@code-cop.org>
2020-11-28 21:25:45 +0100

    Rename classes to start with upper case letter.

--- React/login-form/src/components/Login.tsx
+++ React/login-form/src/components/Login.tsx
@@ -1,32 +1,32 @@
 import React from "react";
-import {AuthenticateResult} from "./Authenticate";
+import { AuthenticateResult } from "./Authenticate";
 
-interface props {
+interface Props {
     authenticate: (userName: string, password: string) => AuthenticateResult;
 }
 
-interface state {
-    userName: string
+interface State {
+    userName: string;
 }
 
-class Login extends React.Component<props, state> {
+class Login extends React.Component<Props, State> {
 
-    constructor(props: Readonly<props>) {
+    constructor(props: Readonly<Props>) {
         super(props);
         this.state = {
-            userName: ""
-        }
+            userName: "",
+        };
     }
 
-    render() {
+    public render() {
         const userName = this.state.userName;
 
         return <div>
             <label htmlFor={"1"}>Phone, email or username</label>
             <input id={"1"}
-                   type={"text"}
-                   value={userName}
-                   onChange={(event) => this.setUserName(event)}
+                type={"text"}
+                value={userName}
+                onChange={(event) => this.setUserName(event)}
             />
 
             <button onClick={() => this.authenticate()}>
@@ -45,7 +45,7 @@ class Login extends React.Component<props, state> {
 
     private setUserName(event: React.ChangeEvent<HTMLInputElement>) {
         const userName: string = event.currentTarget.value;
-        this.setState({userName: userName});
+        this.setState({ userName });
     }
 }
 
