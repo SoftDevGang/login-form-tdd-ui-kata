8486b3caafbb079514415ae2c46bd74803ac666f
Thomas Sundberg <thomas@thinkcode.se>
2020-12-02 12:55:24 +0100

    Refactored the functional component App into a class component App

--- React/login-form/src/App.tsx
+++ React/login-form/src/App.tsx
@@ -1,13 +1,13 @@
-// import React from 'react';
 import './App.css';
 import Login from "./components/Login";
 import logo from './logo.svg';
+import React from "react";
 
-function App() {
-    return (
-        <div className="App">
+class App extends React.Component<any> {
+    render() {
+        return <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     Welcome to Clean Code Center!
                 </p>
@@ -20,11 +20,11 @@ function App() {
                     Learn React
                 </a>
             </header>
-            <Login authenticate={(userName: string, password: string) => {
-                return { success: false, message: "" };
-            }} />
+            <Login failedLogin={false} authenticate={(userName: string, password: string) => {
+                return {success: false, message: ""};
+            }}/>
         </div>
-    );
+    }
 }
 
 export default App;
