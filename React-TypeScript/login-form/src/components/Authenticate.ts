7c25090c12183cbd957dacc041a625b3e57df602
Peter Kofler <peter.kofler@code-cop.org>
2020-12-18 00:05:00 +0100

    Sample Authentication backend to use for kata in TS.

---
+++ React-TypeScript/login-form/src/components/Authenticate.ts
@@ -0,0 +1,25 @@
+const serverUrl = 'http://localhost/api/v1/login';
+
+export interface AuthenticateResult {
+    success: boolean;
+    message?: string;
+}
+
+export async function authenticate(userName: string, password: string): Promise<AuthenticateResult> {
+    const payload = { userName, password };
+
+    return fetch(serverUrl, {
+        method: "POST",
+        body: JSON.stringify(payload)
+    }).
+        then(
+            res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                return { success: false, message: res.statusText };
+            },
+            error => {
+                return { success: false, message: "" + error };
+            });
+}
