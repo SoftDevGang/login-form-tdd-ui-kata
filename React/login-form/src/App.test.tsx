8e9424bcd73827e9e69dc4978182767364d1e260
Peter Kofler <peter.kofler@code-cop.org>
2020-12-17 23:01:10 +0100

    Red: Display error dialog on failed login.

--- React/login-form/src/App.test.tsx
+++ React/login-form/src/App.test.tsx
@@ -4,12 +4,15 @@ import userEvent from "@testing-library/user-event";
 import { rest } from 'msw'
 import { setupServer } from 'msw/node';
 
+let statusCode = 200;
+let body = { success: false, message: "" };
+
 const server = setupServer(
     rest.post('/api/v1/login', (req, res, ctx) => {
         return res(
-            ctx.status(200),
+            ctx.status(statusCode),
             ctx.set('Content-Type', 'application/json'),
-            ctx.json({ success: true, message: "" })
+            ctx.json(body)
         );
     }),
 );
@@ -40,6 +43,7 @@ test('See the login dialog', () => {
 
 // 4. wire the authentication callback
 test('See welcome message on a successful login', async () => {
+    body = { success: true, message: "" };
     render(<App />);
     const userName = screen.getByLabelText("Phone, email or username");
     userEvent.type(userName, "Bob");
@@ -51,6 +55,7 @@ test('See welcome message on a successful login', async () => {
     await sleep(50);
 
     const welcome = screen.getByLabelText("Welcome message");
+    expect(welcome).toBeInTheDocument(); // generic for just checking existence
     expect(welcome).toHaveTextContent("Welcome Bob!");
 
     // reuse test - do not show Login component on success
@@ -61,5 +66,29 @@ function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+test('See error dialog on a failed login', async () => {
+    body = { success: false, message: "" };
+    render(<App />);
+
+    // no error dialog at startup
+    expect(screen.queryByLabelText("Failed login")).toBeNull();
+
+    const userName = screen.getByLabelText("Phone, email or username");
+    userEvent.type(userName, "Bob");
+    const password = screen.getByLabelText("Password");
+    userEvent.type(password, "secretPassword");
+
+    const loginButton: HTMLElement = screen.getByText("Login");
+    userEvent.click(loginButton);
+    await sleep(50);
+
+    // login is still here
+    const element = screen.getByLabelText("Phone, email or username");
+    expect(element).toBeInTheDocument();
+
+    // and it displays error message
+    const errorMessage = screen.getByLabelText("Failed login");
+    expect(errorMessage).toBeInTheDocument(); // generic for just checking existence
+});
+
 // - see the username in welcome message - variant
-// - Show error message at login failure
