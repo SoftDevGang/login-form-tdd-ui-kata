070eddf1b15177024f017c72119d4a12393f955f
Peter Kofler <peter.kofler@code-cop.org>
2020-12-17 22:37:16 +0100

    Add missing test steps, not relevant for the research here.

--- React/login-form/src/components/Authenticate.test.ts
+++ React/login-form/src/components/Authenticate.test.ts
@@ -2,12 +2,13 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node';
 import { authenticate, AuthenticateResult } from "./Authenticate";
 
+let statusCode = 200;
+let body = { success: false, message: "" };
+
 const server = setupServer(
     rest.post('/api/v1/login', (req, res, ctx) => {
-        let body = { success: true, message: "I was called" };
-        // todo add error situations and login failures with more tests
         return res(
-            ctx.status(200),
+            ctx.status(statusCode),
             ctx.set('Content-Type', 'application/json'),
             ctx.json(body)
         );
@@ -18,9 +19,15 @@ afterEach(() => server.resetHandlers());
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
-test('Authenticate calls backend as expected', async () => {
+// 3. drive backend service with integrated web server
+test('Authenticate calls backend endpoint', async () => {
+    body = { success: true, message: "I was called" };
+
     const actual: AuthenticateResult = await authenticate("Bob", "Bobs password");
 
     expect(actual.success).toBeTruthy();
     expect(actual.message).toBe("I was called");
 });
+
+// 3.1 check for proper POST request containing username and password
+// 3.2 add login failures and error situations
